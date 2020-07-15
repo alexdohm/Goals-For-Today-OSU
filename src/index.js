42a1c890a9ad9b46559760b926316f08712d6d5c
Emre Ercelebi <30380456+emreercelebi@users.noreply.github.com>
2020-07-14 23:19:26 -0400

    add redux persistor

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { persistGate, PersistGate} from 'redux-persist/lib/integration/react';
 
 import CreateAccountPage from './js/CreateAccountPage.js';
 import LoginPage from './js/Loginpage.js';
@@ -25,10 +27,17 @@ const App = () => {
   );
 }
 
+const store = configureStore();
+const persistor = persistStore(store);
+
 if (wrapper) {
   ReactDOM.render(
-    <Provider store={configureStore()}>
-      <App />
+    <Provider store={store}>
+      <PersistGate 
+        loading={<div>Loading...</div>}
+        persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>,
     wrapper
   );
--- src/js/redux/store.js
+++ src/js/redux/store.js
@@ -1,9 +1,23 @@
 import { createStore, combineReducers } from 'redux';
+import { persisteReducer, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import { toDos } from './reducers';
 
 const reducers = {
   toDos
 };
-const rootReducer = combineReducers(reducers);
 
-export const configureStore = () => createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2
+}
+
+const rootReducer = combineReducers(reducers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const configureStore = () => createStore(
+  persistedReducer, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
