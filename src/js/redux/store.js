6ae3eebefafc114a9a94c59a5cc9b57f4c02f220
Emre Ercelebi <30380456+emreercelebi@users.noreply.github.com>
2020-07-19 12:24:37 -0400

    disable redux extension

--- src/js/redux/store.js
+++ src/js/redux/store.js
@@ -19,12 +19,4 @@ const persistConfig = {
 const rootReducer = combineReducers(reducers);
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const configureStore = () =>
-  createStore(
-    persistedReducer,
-    compose(
-      applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
+export const configureStore = () => createStore(persistedReducer, compose(applyMiddleware(thunk)))
