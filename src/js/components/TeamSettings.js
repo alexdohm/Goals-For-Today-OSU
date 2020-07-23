51045a63a1671451717d0476fa5512c554f38d5a
Emre Ercelebi <30380456+emreercelebi@users.noreply.github.com>
2020-07-22 21:56:37 -0400

    populate team options on settings page

--- src/js/components/TeamSettings.js
+++ src/js/components/TeamSettings.js
@@ -3,10 +3,7 @@ import { Button, Form, Select } from "semantic-ui-react";
 
 import { FormFieldHelper } from "./common/helpers";
 
-const teamOptions = [
-  { key: "1", value: "1", text: "Neutron" },
-  { key: "2", value: "2", text: "Proton" },
-];
+
 
 class TeamSettings extends Component {
   constructor(props) {
@@ -19,6 +16,14 @@ class TeamSettings extends Component {
     this.handleTeamCreateChange = this.handleTeamCreateChange.bind(this);
     this.handleTeamRequest = this.handleTeamRequest.bind(this);
     this.handleTeamCreate = this.handleTeamCreate.bind(this);
+    this.teamOptions = [];
+    for (const team of this.props.teams) {
+      this.teamOptions.push({
+        key: team.team_id,
+        value: team.team_id,
+        text: team.team_name
+      })
+    }
   }
 
   handleTeamCreateChange() {
@@ -45,7 +50,7 @@ class TeamSettings extends Component {
         <div className="Settings-teamWrapper">
           <Form className="Settings-teamForm">
             <div className="Settings-teamSelect">
-              <Select placeholder="Teams" options={teamOptions} />
+              <Select placeholder="Teams" options={this.teamOptions} />
               <Button primary onClick={this.handleTeamRequest}>
                 Request
               </Button>
