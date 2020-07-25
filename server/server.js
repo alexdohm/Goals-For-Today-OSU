edb53d443db930e8f4c0ffa97e1307e9dcb8ad55
Alex Dohm <dohma@oregonstate.edu>
2020-07-25 15:58:47 +0200

    email setup complete

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const Email = require("./handlers/email.js");
-Email.biDailyUpdate();
+// Email.biDailyUpdate();
 
 // future use - app.use(passport.initialize())
 // future use - app.use(passport.session())
