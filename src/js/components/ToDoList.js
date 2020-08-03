60ed5f151e876bec1762bf7c67e8eca882c426b2
Emre Ercelebi <30380456+emreercelebi@users.noreply.github.com>
2020-08-03 00:08:42 -0400

    only allow user to add a goal if the selected date is today

--- src/js/components/ToDoList.js
+++ src/js/components/ToDoList.js
@@ -174,7 +174,7 @@ class ToDoList extends Component {
             )
             .flat()}
         </div>
-        {this.props.selectedUserId == this.props.currentUserId ? (
+        {this.props.selectedUserId == this.props.currentUserId && isSameDay(this.state.date, new Date()) ? (
           <div className="ToDoList-addButtonContainer">
             <IconButton
               baseClass="ToDoList"
